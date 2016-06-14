0ea6bca45e3651bb38a1df8ee97aa054f90decc2
Mr.Q <qianlishun@mrq-macbook-pro.local>
2016-06-14 16:25:15 +0800

    Initial Commit

---
+++ Session/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Session
+//
+//  Created by Mr.Q on 16/6/14.
+//  Copyright © 2016年 QLS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Session/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Session
+//
+//  Created by Mr.Q on 16/6/14.
+//  Copyright © 2016年 QLS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
